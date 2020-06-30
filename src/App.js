e4f9fbff0153fa603abb59772e38d8b38d6835b4
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-06-30 07:58:35 -0700

    faded scrolling is working

--- src/App.js
+++ src/App.js
@@ -1,15 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 //components
 import Scores from './components/Scores';
 
+
+
+
+
 function App() {
+
+  const [ position, setPosition ] = useState(window.pageYOffset)
+  const listenToScroll = () => {
+    const winScroll =
+      document.body.scrollTop || document.documentElement.scrollTop
+  
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight
+
+    const scrolled = winScroll / height
+
+  
+    setPosition({
+      scrolled,
+    })
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', listenToScroll)
+  },[])
+
+
+  console.log(position)
+
+
+
+//   this.setState({
+//     post: post,
+//     theposition: window.pageYOffset
+// });
+
   return (
     <div className="App">
-      <header className="App-header">
-      <Scores />
+      <header style={{opacity: position.scrolled * 10 }} className="App-header">
+      <Scores  />
       </header>
+      {/* <header style={{opacity: position.scrolled}} className="App-header">
+      <h3>HELLO THERE SECOND BOX</h3>
+      </header> */}
     </div>
   );
 }
